e12ddeb60a9f9c124f1e393b024cb896371ff74c
Havoc <67231401+Programmer41@users.noreply.github.com>
2025-12-15 19:56:25 -0700

    Refine MecanumDrive parameters for better performance
    Updated drive model and feedforward parameters for Mecanum drive.

--- TeamCode/src/main/MecanumDrive.java
+++ TeamCode/src/main/MecanumDrive.java
@@ -63,12 +63,12 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.LEFT;
 
         // drive model parameters
-        public double inPerTick = 1;
-        public double lateralInPerTick = inPerTick;
-        public double trackWidthTicks = 0;
+        public double inPerTick = 0.0019667721518987;
+        public double lateralInPerTick = 0.0016218709206881046;
+        public double trackWidthTicks = 7357.4903214584;
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
+        public double kS = 0.9798076685553792;
+        public double kV = 0.00035002494612470895;
         public double kA = 0;
 
         // path profile parameters (in inches)
@@ -228,6 +228,11 @@ public final class MecanumDrive {
         leftBack = hardwareMap.get(DcMotorEx.class, "BL");
         rightBack = hardwareMap.get(DcMotorEx.class, "BR");
         rightFront = hardwareMap.get(DcMotorEx.class, "FR");
+
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
