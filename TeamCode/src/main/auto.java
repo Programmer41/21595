7be29eee562ea02112ff01278e069b61a38c33f8
Havoc <67231401+Programmer41@users.noreply.github.com>
2026-02-05 12:15:36 -0700

    Add servo motors to auto class
    Added two servo motors and initialized their positions.

--- TeamCode/src/main/auto.java
+++ TeamCode/src/main/auto.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 @Autonomous(name = "auto", group = "Linear Opmode")
@@ -15,6 +16,9 @@ public class auto extends LinearOpMode {
     private DcMotor FR = null;
     private DcMotor BR = null;
 
+    private Servo servoOne = null;
+    private Servo servoTwo = null;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -24,10 +28,20 @@ public class auto extends LinearOpMode {
         FR = hardwareMap.get(DcMotor.class, "FR");
         BR = hardwareMap.get(DcMotor.class, "BR");
 
+        servoOne = hardwareMap.get(Servo.class, "servoOne");
+        servoTwo = hardwareMap.get(Servo.class, "servoTwo");
+
         FL.setDirection(DcMotorSimple.Direction.REVERSE);
         BL.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        servoOne.setPosition(0.50);
+        servoTwo.setPosition(0.75);
+
         waitForStart();
+
+        servoOne.setPosition(0.50);
+        servoTwo.setPosition(0.75);
+        
             FL.setPower(0.5);
             BL.setPower(0.5);
             FR.setPower(0.5);
